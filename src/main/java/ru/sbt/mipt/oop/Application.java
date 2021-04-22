e12d22a617231a9806b327b177e9fe70c6a2e1e2
Anton Ivanov <me@antonivanov.net>
2021-04-22 20:37:13 +0300

    Renamed method.

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -39,13 +39,13 @@ public class Application {
                 // событие от двери
                 for (Room room : smartHome.getRooms()) {
                     for (Door door : room.getDoors()) {
-                        if (door.getId().equals(event.getObjectId())) {
+                        if (door.getMyId().equals(event.getObjectId())) {
                             if (event.getType() == DOOR_OPEN) {
                                 door.setOpen(true);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was opened.");
+                                System.out.println("Door " + door.getMyId() + " in room " + room.getName() + " was opened.");
                             } else {
                                 door.setOpen(false);
-                                System.out.println("Door " + door.getId() + " in room " + room.getName() + " was closed.");
+                                System.out.println("Door " + door.getMyId() + " in room " + room.getName() + " was closed.");
                                 // если мы получили событие о закрытие двери в холле - это значит, что была закрыта входная дверь.
                                 // в этом случае мы хотим автоматически выключить свет во всем доме (это же умный дом!)
                                 if (room.getName().equals("hall")) {
